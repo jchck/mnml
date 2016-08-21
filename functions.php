be9562129101334ab92907af169be086cacbb79b
jchck <hey@justinchick.com>
2016-08-20 21:22:41 -0500

    typo

--- functions.php
+++ functions.php
@@ -52,7 +52,7 @@ function assets(){
 	wp_enqueue_style( 'css', get_template_directory_uri() . '/dest/mnml.css', false, null );
 }
 
-add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '//assets' );
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\assets' );
 
 
 /**
