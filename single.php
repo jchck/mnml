c5a08a6a6fa1adb4a01041da9f7247ad77c0f78f
jchck <hey@justinchick.com>
2016-08-21 13:41:51 -0500

    single

---
+++ single.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+<div class="px3 py4" style="min-height: 85vh">
+
+	<?php while (have_posts()) : the_post(); ?>
+
+		<h1 class="measure mb0"><a href="<?= esc_url(home_url('/')); ?>"><?php bloginfo( 'name' ); ?></a> : <?php the_title(); ?></h1>
+
+		<time class="caps h6 gray"><?= get_the_date(); ?></time>
+
+		<?php the_content(); ?>
+
+	<?php endwhile; ?>
+</div>
+
+<?php get_footer(); ?>
