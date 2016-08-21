3ca5187980af3ad840056854aaf34edfd7955c57
jchck <hey@justinchick.com>
2016-08-21 12:34:21 -0500

    update no posts found message

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 
 	<?php else : ?>
 
-		<p>Sorry but no posts were found :(</p>
+		<h1 class="measure"><a href="<?= esc_url(home_url('/')); ?>"><?php bloginfo( 'name' ); ?></a> : sorry but no posts were found :(</h1>
 
 	<?php endif; ?>
 
